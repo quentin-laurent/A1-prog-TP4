9104c17cbc948ecdfd7f10757e57eeb9a1f4f034
quentinlaurent <quentin.laurent.sup@gmail.com>
2022-11-08 17:35:34 +0100

    removed baseDamage attribute from Arrow item

--- src/main/java/com/isep/rpg/Arrow.java
+++ src/main/java/com/isep/rpg/Arrow.java
@@ -2,19 +2,9 @@ package com.isep.rpg;
 
 public class Arrow extends Item
 {
-    // Attributes
-    private final int baseDamage;
-
     // Constructor
     public Arrow(String name, int baseDamage)
     {
         super(name);
-        this.baseDamage = baseDamage;
-    }
-
-    // Getters & Setters
-    public int getBaseDamage()
-    {
-        return this.baseDamage;
     }
 }
