42ce5d6f852c4d4c52adffac980f294bab7deac6
quentinlaurent <quentin.laurent.sup@gmail.com>
2022-11-29 12:11:48 +0100

    added displayDefendMessage() method and moved documentation to OutputManager interface

--- src/main/java/com/isep/utils/ConsoleOutput.java
+++ src/main/java/com/isep/utils/ConsoleOutput.java
@@ -9,11 +9,6 @@ import java.util.Collection;
 
 public class ConsoleOutput implements OutputManager
 {
-    /**
-     * Displays the list of the {@link Hero}es in the {@link Game}.
-     * For each {@link Hero}, indicates if it's dead.
-     * @param heroes The list of {@link Hero}es of the {@link Game}.
-     */
     @Override
     public void displayHeroes(Collection<Hero> heroes)
     {
@@ -29,11 +24,6 @@ public class ConsoleOutput implements OutputManager
         System.out.println(s);
     }
 
-    /**
-     * Displays the list of all {@link Enemy} instances in the {@link Game}.
-     * For each {@link Enemy}, indicates if it's dead.
-     * @param enemies The list of all {@link Enemy} instances in the {@link Game}.
-     */
     @Override
     public void displayEnemies(Collection<Enemy> enemies)
     {
@@ -49,34 +39,25 @@ public class ConsoleOutput implements OutputManager
         System.out.println(s);
     }
 
-    /**
-     * Displays the title of the current stage
-     * @param stageNumber The number of the current stage
-     */
     @Override
     public void displayStageTitle(int stageNumber)
     {
         System.out.printf("==== STAGE %d ====%n", stageNumber);
     }
 
-    /**
-     * Displays the end screen of the current {@link Game}
-     */
     @Override
     public void displayEndScreen()
     {
         System.out.println("==== END ====");
     }
 
-    /**
-     * Displays a message containing the information of the {@link Hero} currently playing
-     * @param hero The {@link Hero} currently plqying
-     */
+    @Override
     public void displayHero(Hero hero)
     {
         System.out.printf("**** Now playing: %s (%d/%d) [%s]****%n",hero.getName(), hero.getHp(), hero.getMaxHP(),hero.getClass().getSimpleName());
     }
 
+    @Override
     public void displayAttackMessage(Combatant attacker, Combatant target, int damage)
     {
         System.out.printf("%s attacks %s !%n", attacker.getName(), target.getName());
@@ -84,5 +65,12 @@ public class ConsoleOutput implements OutputManager
             System.out.printf("%s: -%dHP%n", target.getName(), damage);
         else
             System.out.printf("%s: -%dHP (dead)%n", target.getName(), damage);
+    }
+
+    @Override
+    public void displayDefendMessage(Combatant combatant, int damageReductionPercentage)
+    {
+        if(damageReductionPercentage != 0)
+            System.out.printf("%s reduced the damage by %d%% !%n", combatant.getName(), damageReductionPercentage);
     }
 }
--- src/main/java/com/isep/utils/OutputManager.java
+++ src/main/java/com/isep/utils/OutputManager.java
@@ -2,21 +2,56 @@ package com.isep.utils;
 
 import com.isep.rpg.Combatant;
 import com.isep.rpg.Enemy;
+import com.isep.rpg.Game;
 import com.isep.rpg.Hero;
 
 import java.util.Collection;
 
 public interface OutputManager
 {
+    /**
+     * Displays the list of the {@link Hero}es in the {@link Game}.
+     * For each {@link Hero}, indicates if it's dead.
+     * @param heroes The list of {@link Hero}es of the {@link Game}.
+     */
     public void displayHeroes(Collection<Hero> heroes);
 
+    /**
+     * Displays the list of all {@link Enemy} instances in the {@link Game}.
+     * For each {@link Enemy}, indicates if it's dead.
+     * @param enemies The list of all {@link Enemy} instances in the {@link Game}.
+     */
     public void displayEnemies(Collection<Enemy> enemies);
 
+    /**
+     * Displays the title of the current stage
+     * @param stageNumber The number of the current stage
+     */
     public void displayStageTitle(int stageNumber);
 
+    /**
+     * Displays a message containing the information of the {@link Hero} currently playing
+     * @param hero The {@link Hero} currently plqying
+     */
     public void displayHero(Hero hero);
 
+    /**
+     * Displays a message indicating who attacked who and the damage inflicted
+     * @param attacker The {@link Combatant} attacking
+     * @param target The {@link Combatant} being attacked
+     * @param damage The damage inflicted
+     */
     public void displayAttackMessage(Combatant attacker, Combatant target, int damage);
 
+    /**
+     * Displays a message indicating the damage reduction percentage of a defending {@link Combatant}
+     * @param combatant The {@link Combatant} defending itself
+     * @param damageReductionPercentage The percentage of damage being reduced
+     */
+    public void displayDefendMessage(Combatant combatant, int damageReductionPercentage);
+
+    /**
+     * Displays the end screen of the current {@link Game}
+     */
     public void displayEndScreen();
 }
