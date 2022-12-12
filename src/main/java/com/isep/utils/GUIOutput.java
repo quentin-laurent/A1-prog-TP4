c53e4c42f96251657e455e7e4e792d9c3081bce4
quentinlaurent <quentin.laurent.sup@gmail.com>
2022-12-12 19:39:30 +0100

    updated GUIOutput and updated documentation
    GUIOutput now implements OutputManager
    exceptions have been added to method bodies, method signatures and documentation

--- src/main/java/com/isep/utils/GUIOutput.java
+++ src/main/java/com/isep/utils/GUIOutput.java
@@ -1,6 +1,109 @@
 package com.isep.utils;
 
-public class GUIOutput
-{
+import com.isep.rpg.*;
+import jdk.jshell.spi.ExecutionControl;
 
+import java.util.Collection;
+
+/**
+ * An implementation of the {@link OutputManager} interface that uses a graphical user interface (a.k.a a GUI)
+ * to display information about the actions happening in the {@link Game}.
+ */
+public class GUIOutput implements OutputManager
+{
+    @Override
+    public void displayHeroes(Collection<Hero> heroes) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayEnemies(Collection<Enemy> enemies) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayStageTitle(int stageNumber) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayRoundTitle(int roundNumber) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayLootMessage() throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayUpgradesTitle() throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayUpgradeMessage(Hero hero) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayHero(Hero hero) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayEnemy(Enemy enemy) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayAttackMessage(Combatant attacker, Combatant target, int damage) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayCastSpellMessage(SpellCaster caster, Combatant target, int damageOrHeal, int manaCost) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayDefendMessage(Combatant combatant, int damageReductionPercentage) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayConsumableUsed(Combatant target, Consumable consumable, int appliedEffectValue) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayItemAddMessage(Hero hero, Item item, int quantity) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayErrorMessage(String message) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
+
+    @Override
+    public void displayEndScreen(boolean victory) throws ExecutionControl.NotImplementedException
+    {
+        throw new ExecutionControl.NotImplementedException("TODO");
+    }
 }
--- src/main/java/com/isep/utils/OutputManager.java
+++ src/main/java/com/isep/utils/OutputManager.java
@@ -5,94 +5,132 @@ import jdk.jshell.spi.ExecutionControl;
 
 import java.util.Collection;
 
+/**
+ * An interface used to display information about the actions happening in the {@link Game}.
+ */
 public interface OutputManager
 {
     /**
-     * Displays the list of the {@link Hero}es in the {@link Game}.
-     * For each {@link Hero}, indicates if it's dead.
-     * @param heroes The list of {@link Hero}es of the {@link Game}.
+     * Displays all the {@link Hero}es contained in the provided list.
+     * @param heroes The list containing the {@link Hero}es to display.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayHeroes(Collection<Hero> heroes);
+    //TODO: change param type to List<Hero>
+    public void displayHeroes(Collection<Hero> heroes) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays the list of all {@link Enemy} instances in the {@link Game}.
-     * For each {@link Enemy}, indicates if it's dead.
-     * @param enemies The list of all {@link Enemy} instances in the {@link Game}.
+     * Displays all the {@link Enemy}s contained in the provided list.
+     * @param enemies The list containing the {@link Enemy}s to display.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayEnemies(Collection<Enemy> enemies);
+    //TODO: change param type to List<Enemy>
+    public void displayEnemies(Collection<Enemy> enemies) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays the title of the current stage
-     * @param stageNumber The number of the current stage
+     * Displays the title of a stage.
+     * @param stageNumber The number of the stage.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayStageTitle(int stageNumber);
+    public void displayStageTitle(int stageNumber) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays the title of the current round
-     * @param roundNumber The number of the current round
+     * Displays the title of a round.
+     * @param roundNumber The number of the round.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayRoundTitle(int roundNumber);
-
-    public void displayLootMessage();
-
-    public void displayUpgradesTitle();
-
-    public void displayUpgradeMessage(Hero hero);
+    public void displayRoundTitle(int roundNumber) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message containing the information of the {@link Hero} currently playing
-     * @param hero The {@link Hero} currently plqying
+     * Displays the title of the "rewards" part of a stage.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayHero(Hero hero);
+    //TODO: rename method to displayLootTitle
+    public void displayLootMessage() throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message containing the information of the {@link Enemy} currently playing
-     * @param enemy The {@link Enemy} currently plqying
+     * Displays the title of the "upgrades" part of a stage.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayEnemy(Enemy enemy);
+    public void displayUpgradesTitle() throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message indicating who attacked who and the damage inflicted
-     * @param attacker The {@link Combatant} attacking
-     * @param target The {@link Combatant} being attacked
-     * @param damage The damage inflicted
+     * Displays a message containing the information of the {@link Hero} being upgraded.
+     * @param hero The {@link Hero} being upgraded.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayAttackMessage(Combatant attacker, Combatant target, int damage);
+    public void displayUpgradeMessage(Hero hero) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message giving details on the cast spell (whether it is an attack spell or a heal spell)
-     * @param caster The {@link SpellCaster} who cast the spell
-     * @param target The {@link Combatant} being targeted by the spell
-     * @param manaCost The amount of mana consumed by the spell
-     * @param damageOrHeal The damage inflicted if the spell is an attack spell or the heal applied is a healing spell
+     * Displays a message containing the information of the {@link Hero} currently playing.
+     * @param hero The {@link Hero} currently playing.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayCastSpellMessage(SpellCaster caster, Combatant target, int damageOrHeal, int manaCost);
+    public void displayHero(Hero hero) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message indicating the damage reduction percentage of a defending {@link Combatant}
-     * @param combatant The {@link Combatant} defending itself
-     * @param damageReductionPercentage The percentage of damage being reduced
+     * Displays a message containing the information of the {@link Enemy} currently playing.
+     * @param enemy The {@link Enemy} currently playing.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayDefendMessage(Combatant combatant, int damageReductionPercentage);
+    public void displayEnemy(Enemy enemy) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message indicating the {@link Consumable} used and its effect
-     * @param target The {@link Combatant} consuming the {@link Consumable}
-     * @param consumable The {@link Consumable} used
+     * Displays a message indicating who attacked who as well as the damage inflicted.
+     * @param attacker The {@link Combatant} attacking.
+     * @param target The {@link Combatant} being attacked.
+     * @param damage The damage inflicted by the attacker to the target.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayConsumableUsed(Combatant target, Consumable consumable, int appliedEffectValue);
-
-    public void displayItemAddMessage(Hero hero, Item item, int quantity);
+    public void displayAttackMessage(Combatant attacker, Combatant target, int damage) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays a message indicating that the inventory of the {@link Hero} currently playing is empty.
+     * Displays a message giving details on the cast spell (whether if it is an attack spell or a healing spell)
+     * @param caster The {@link SpellCaster} who cast the spell.
+     * @param target The {@link Combatant} targeted by the spell.
+     * @param manaCost The amount of mana consumed by the spell.
+     * @param damageOrHeal The damage inflicted if the spell is an attack spell or the healing applied if
+     *                     the spell is a healing spell
+     * @throws ExecutionControl.NotImplementedException
+     */
+    public void displayCastSpellMessage(SpellCaster caster, Combatant target, int damageOrHeal, int manaCost) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Displays a message indicating the percentage of damage reduced by a defending {@link Combatant}.
+     * @param combatant The {@link Combatant} defending itself.
+     * @param damageReductionPercentage The damage reduction percentage.
+     * @throws ExecutionControl.NotImplementedException
+     */
+    public void displayDefendMessage(Combatant combatant, int damageReductionPercentage) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Displays a message indicating the {@link Consumable} used and its effect.
+     * @param target The {@link Combatant} consuming the {@link Consumable}.
+     * @param consumable The {@link Consumable} used.
+     * @throws ExecutionControl.NotImplementedException
+     */
+    public void displayConsumableUsed(Combatant target, Consumable consumable, int appliedEffectValue) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Displays a message containing information about the {@link Item} (and its quantity) awarded to a {@link Hero}.
+     * @param hero The {@link Hero} receiving the {@link Item}.
+     * @param item The {@link Item} awarded to the {@link Hero}.
+     * @param quantity The {@link Item}'s quantity.
+     * @throws ExecutionControl.NotImplementedException
+     */
+    //TODO: rename method to displayRewardMessage
+    public void displayItemAddMessage(Hero hero, Item item, int quantity) throws ExecutionControl.NotImplementedException;
+
+    /**
+     * Displays the provided error message.
+     * @param message The error message to display.
      * @throws ExecutionControl.NotImplementedException
      */
     public void displayErrorMessage(String message) throws ExecutionControl.NotImplementedException;
 
     /**
-     * Displays the end screen of the current {@link Game}
-     * @param victory A boolean indicating if the player won
+     * Displays the end screen of the {@link Game}.
+     * @param victory A boolean indicating if the player won.
+     * @throws ExecutionControl.NotImplementedException
      */
-    public void displayEndScreen(boolean victory);
+    public void displayEndScreen(boolean victory) throws ExecutionControl.NotImplementedException;
 }
